cdd4272d6b546de9ee4a7c96c95e44707b7df794
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2024-04-06 16:52:47 -0600

    pasarela de pago finalizada

--- Proyecto-final/src/controllers/payment_controllers.js
+++ Proyecto-final/src/controllers/payment_controllers.js
@@ -1,18 +1,25 @@
 import PaymentService from '../services/payment-services.js'
+import { CartRepository } from '../services/index.js';
 
 export const cartToPay= async (req, res) => {
+
+  const {user}= req.user;
+  const cartFound = await CartRepository.getCartById(user._id);
+  let totalAmount=0;
+
+  for (const iterator of cartFound.products) {
+    totalAmount += iterator.product.price * iterator.quantity
+  }
 
 const stripe = new PaymentService()
 
   try {
     const charge = {
-      amount: 2000,
+      amount: totalAmount * 100,
       currency: 'mxn',
       description: 'Descripción del producto o servicio',
       payment_method_types: ['card'],
     };
-
-    console.log(charge);
 
     const result = await stripe.createPaymentIntent(charge);
 
